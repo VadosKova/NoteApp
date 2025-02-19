685e9773358e1c48a5518782a627dd6a78f151ce
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-02-19 11:23:52 +0200

    Added method for adding note

--- NoteOperations.py
+++ NoteOperations.py
@@ -1,3 +1,15 @@
+from Notes import Note
+
 class NoteOperations:
     def __init__(self):
         self.notes = []
+
+    def add_note(self, id, title, content):
+        if any(note.get_id() == id for note in self.notes):
+            print("Заметка с таким ID уже есть")
+            return
+
+        note = Note(id, title, content)
+        self.notes.append(note)
+        print(f"Note with ID '{id}' added")
+
