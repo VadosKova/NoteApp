07d7f74db22099d3b90bd2f911f01b98bcb83525
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-02-19 12:11:33 +0200

    Added method for saving to file and loading from file

--- NoteSaving.py
+++ NoteSaving.py
@@ -1,3 +1,4 @@
+from Notes import Note
 import os
 
 class NoteSaving:
@@ -6,3 +7,30 @@ class NoteSaving:
         if not os.path.exists(self.file):
             with open(self.file, 'w') as file:
                 print("Файл не найден. Создан новый")
+
+    def save_notes_to_file(self, notes):
+        try:
+            with open(self.file, 'w') as file:
+                for note in notes:
+                    file.write(f"ID: {note.get_id()}, Title: {note.title}, Content: {note.content};\n")
+            print(f"Notes saved to {self.file}")
+        except Exception:
+            print(f"Error")
+
+    def load_notes_from_file(self):
+        notes = []
+        try:
+            with open(self.file, 'r') as file:
+                content = file.read().strip().split("\n")
+                for note_data in content:
+                    note_lines = note_data.split(";")
+                    if len(note_lines) == 3:
+                        id, title, content = note_lines
+                        notes.append(Note(id, title, content))
+        except FileNotFoundError:
+            print("Empty")
+
+        except Exception:
+            print("Error")
+
+        return notes
